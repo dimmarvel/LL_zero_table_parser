ffc3b38673cba7a2aba60f38fe63b792e7a8629d
Matsiukhov Dzmitry <dimmarvel@mail.ru>
2020-01-14 21:31:42 +0300

    rewrite parser.cpp

--- LL_zero/revritePARSERH.h
+++ LL_zero/revritePARSERH.h
@@ -1,23 +1,22 @@
 #ifndef _PARSER_H_
 #define _PARSER_H_
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include "lex_analyzer.h"
 
-#define PARSER_TABLE_ROW 6
-#define PARSER_TABLE_COLS 7
+#define PARSE_TABLE_ROWS 6
+#define PARSE_TABLE_COLS 7
+
+#define TODO_TABLE_COL_LEN 4
+#define TODO_TABLE_COL_LEN 8
 
 #define ERROR -1
 #define EMPTY -1
 
 #define PARSE_STACK_SIZE 10
 #define VALUE_STACK_SIZE 10
-
-#define TODO_TABLE_COL_LEN 4
-#define TODO_TABLE_ROW_LEN 8
 
 enum NONTERMINAL {
 	STMT = 10,
@@ -28,18 +27,18 @@ enum NONTERMINAL {
 	FACT = 15
 };
 enum ACTION {
-	PLUS_ACT = 100,
-	STAR_ACT = 101,
+	PLUC_ACT = 100,
+	STAR_ACT = 101
 };
 
 int parse_input(char* input_str);
 
 static void init_parse_table();
-static void init_todo_table();
-static int parse_stack_pop();
-static void parse_stack_push(int value);
-static int value_stack_push();
-static void value_stack_push(int value);
+static int init_todo_table();
+static void parse_stack_pop();
+static int parse_stack_push(int value);
+static void value_stack_pop();
+static  void value_stack_push(int value);
 static void init();
 static enum NONTERMINALS index_to_nonterminal(int index);
 static int nonterminal_to_index(NONTERMINAL nonterm);
